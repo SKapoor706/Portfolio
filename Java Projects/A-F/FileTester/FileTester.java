b98d72d9df8fcc409e4e84c753dc46ffece3ddac
amanmibra <amanmibra@gmail.com>
2015-08-08 21:22:23 -0400

    Update FileTester.java

--- Java Projects/A-F/FileTester/FileTester.java
+++ Java Projects/A-F/FileTester/FileTester.java
@@ -4,16 +4,16 @@ public class FileTester
 {
 public static void main(String args[])throws IOException
 {
-    Scanner phoenixizdum = new Scanner(new File("D:\\IB CS\\temp_Aman\\MyData.in.txt"));
+    Scanner input = new Scanner(new File("D:\\IB CS\\temp_Aman\\MyData.in.txt"));
     int maxIndx = -1; //-1 so when we increment below, first index is 0
 String text[] = new String[1000]; //to be safe, declare more than we need
-while(phoenixizdum.hasNext( ))
+while(input.hasNext( ))
 {
 maxIndx++;
-text[maxIndx] = phoenixizdum.nextLine( ) ;
+text[maxIndx] = input.nextLine( ) ;
 }
 //maxIndx is now the highest index of text[], -1 if no text lines
-phoenixizdum.close( ); //We opened a file above so close it when finished.
+input.close( ); //We opened a file above so close it when finished.
 for(int j = 0; j <= maxIndx; j++)
 {
 System.out.println( text[j] );
